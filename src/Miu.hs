5cef6c14b8c2d020851fbe2c6ff8b0b470bd2650
Petre Mierlutiu <petrem@users.noreply.github.com>
2020-11-01 11:31:00 +0200

    remove redundant signs, simplify stuff

--- src/Miu.hs
+++ src/Miu.hs
@@ -21,7 +21,7 @@ import Lib
 
 
 data Letter = M | I | U deriving (Eq, Show)
-data Theorem = Theorem [Letter] deriving (Eq, Show)
+newtype Theorem = Theorem [Letter] deriving (Eq, Show)
 
 readLetter :: Char -> Letter
 readLetter c = case c of 'M' -> M
@@ -41,24 +41,24 @@ readTheorem = Theorem . readLetters
 -- (I) MxI -> MxIU
 rule1 :: Theorem -> Either String Theorem
 rule1 (Theorem t@(M:xs)) | last xs == I = Right $ Theorem $ t ++ [U]
-rule1 t = Left $ "Cannot apply (I) to " ++ (show t)
+rule1 t = Left $ "Cannot apply (I) to " ++ show t
 
 -- (II) Mx -> Mxx
 rule2 :: Theorem -> Either String Theorem
-rule2 (Theorem t@(M:xs)) = Right $ Theorem $ (t ++ xs)
-rule2 t = Left $ "Cannot apply (II) to " ++ (show t)
+rule2 (Theorem t@(M:xs)) = Right $ Theorem (t ++ xs)
+rule2 t = Left $ "Cannot apply (II) to " ++ show t
 
 -- (III) MxIIIy -> MxUy
 rule3 :: Int -> Theorem -> Either String Theorem
 rule3 pos (Theorem (M:xs))
-  | getSegment (pos-1) 3 xs == (Right $ readLetters "III") = Theorem <$> (Right (M:) <*> ((replaceSegment (pos-1) 3 xs $ readLetters "U")))
-rule3 pos t = Left $ "Cannot apply (III) to " ++ (show t) ++ " at " ++ show pos
+  | getSegment (pos-1) 3 xs == (Right $ readLetters "III") = Theorem <$> (Right (M:) <*> replaceSegment (pos-1) 3 xs (readLetters "U"))
+rule3 pos t = Left $ "Cannot apply (III) to " ++ show t ++ " at " ++ show pos
 
 -- (IV) MxUUy -> Mxy
 rule4 :: Int -> Theorem -> Either String Theorem
-rule4 pos (Theorem t@(M:xs))
-  | getSegment (pos-1) 2 xs == (Right $readLetters "UU") = Theorem <$> (Right (M:) <*> replaceSegment (pos-1) 2 xs [])
-rule4 pos t = Left $ "Cannot apply (IV) to " ++ (show t) ++ " at " ++ show pos
+rule4 pos (Theorem (M:xs))
+  | getSegment (pos-1) 2 xs == (Right $ readLetters "UU") = Theorem <$> (Right (M:) <*> replaceSegment (pos-1) 2 xs [])
+rule4 pos t = Left $ "Cannot apply (IV) to " ++ show t ++ " at " ++ show pos
 
 
 deriveNextTheorems :: Theorem -> Either String [Theorem]
@@ -70,7 +70,8 @@ deriveNextTheorems t@(Theorem ls) = sequenceA $ filter isRight $ rules <*> pure 
                        ]
 
 deriveTheorems :: Either String [Theorem] -> Either String [[Theorem]]
-deriveTheorems ts = join $ (traverse deriveNextTheorems) <$> ts
+--deriveTheorems ts = join $ (traverse deriveNextTheorems) <$> ts
+deriveTheorems ts = traverse deriveNextTheorems =<< ts
 
 
 -- TODO:
